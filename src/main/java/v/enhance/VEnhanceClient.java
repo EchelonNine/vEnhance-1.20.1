b3c8a6c0ebb7f08e5202fef8b3035f6ee6cc28cc
EchelonNine <dakotasherman7919@gmail.com>
2023-07-14 17:05:38 -0500

    Initial commit

---
+++ src/main/java/v/enhance/VEnhanceClient.java
@@ -0,0 +1,10 @@
+package v.enhance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VEnhanceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
